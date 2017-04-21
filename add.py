a721312cb3cedd712b0df5d528075a0def25dbcc
avanish42 <avanish.singh42@gmail.com>
2017-04-22 00:16:36 +0530

    first commit

---
+++ add.py
@@ -0,0 +1,8 @@
+print("enter first number:")
+firstNum= int(input())
+secondNum=  int (input("enter second number: "))
+thirdNum= firstNum+secondNum;
+
+print(" first Number :{0} Second Number : {1} Sum is  {2} ".format(firstNum,secondNum,thirdNum))
+
+print("test")
---
+++ demo1.py
@@ -0,0 +1,10 @@
+def main():
+    print("what is your name")
+    myName = input()
+    print(myName)
+    print ("heloo {0}!".format(myName))
+    yourAge= int( input(" What is your age :"))           
+    print ("Hello {0}.You are a young {1}".format(myName,yourAge))
+           
+
+
